03b7a7f376c2acf1c13da8ad420d9d371a9f7d3b
777388 <96343159+777388@users.noreply.github.com>
2022-09-11 08:20:34 -0700

    Update DamnSmallFuzzer.py

--- DamnSmallFuzzer.py
+++ DamnSmallFuzzer.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 
-print("Usage: python3 fuzzer.py path/to/iplist.txt output.txt path/to/fuzz.txt")
+print("Usage: python fuzzer.py path/to/iplist.txt output.txt path/to/fuzz.txt")
 input1 = sys.argv[1]
 output1 = open(sys.argv[2], 'w')
 fuzz = sys.argv[3]
@@ -11,11 +11,11 @@ with open(input1, 'r') as filer:
         with open(fuzz, 'r') as filee:
             for fuzzline in filee:
                 process = os.popen("curl -so /dev/null https://"+line.rstrip()+"/"+fuzzline.rstrip()+" -w %{http_code}").read()
-                if process == '\'200\'' or process == '\'403\'':
+                if process == '200' or process == '403':
                     print("https://"+line.rstrip()+"/"+fuzzline.rstrip()+"    "+process+"\r")
                     print("https://"+line.rstrip()+"/"+fuzzline.rstrip()+"    "+process+"\r", file=output1)
                 process = os.popen("curl -so /dev/null http://"+line.rstrip()+"/"+fuzzline.rstrip()+" -w %{http_code}").read()
-                if process == '\'200\'' or process == '\'403\'':
+                if process == '200' or process == '403':
                     print("http://"+line.rstrip()+"/"+fuzzline.rstrip()+"     "+process+"\r")
                     print("http://"+line.rstrip()+"/"+fuzzline.rstrip()+"     "+process+"\r", file=output1)
                 
